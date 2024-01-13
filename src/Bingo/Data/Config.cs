fbb0ee5200b9a4b813e7373943e1f6d3862cb1ae
hennie <henniefrancis@gmail.com>
2024-01-14 00:51:56 +0200

    Removed Pages Updated Fav Icon Added Config

---
+++ src/Bingo/Data/Config.cs
@@ -0,0 +1,9 @@
+﻿namespace Bingo.Config.Images
+{
+    public static class System
+    {
+        private static string content = "wwwroot/content/system.json";
+
+        public static string FavIcon { get; set; } = new ConfigurationBuilder().AddJsonFile(content).Build().GetSection("Images:System")["FavIcon"];
+    }
+}
